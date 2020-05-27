6a1bec24237d3637548fd2cd5b03aae54919ced0
HemalathaSanil <hemalathajsanil33@gmail.com>
2020-05-27 17:20:40 +0530

    Ninth

---
+++ 27 May 2020/script1.py
@@ -0,0 +1,11 @@
+from tkinter import *
+window=Tk()
+def km_to_miles():
+    print("Success")
+b1=Button(window,text="Execute",command=km_to_miles)
+b1.grid(row=0,column=0)
+e1=Entry(window)
+e1.grid(row=0,column=1)
+t1=Text(window,height=1,width=20)
+t1.grid(row=0,column=2)
+window.mainloop()
